5056e0607eaeaeb556a03d97b25a86cf0839e750
ravens675 <ravens1706@gmail.com>
2026-07-27 13:39:55 +0700

    Increase request body limit to 50mb to fix 413 error

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:5174', ...(process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : [])],
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(async () => {
